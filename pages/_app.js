2759f5640a5b2beb85f37c1ec65ff8d74e064583
arturkorra <arturi25turi@gmail.com>
2021-11-04 16:25:05 +0100

    added first pages and components

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,27 @@
 import '../styles/globals.css'
+import '../public/css/bootstrap.min.css'
+import '../public/css/animate.min.css'
+import '../public/css/style.css'
+import '../public/css/owl.carousel.min.css'
+import '../public/css/jquery.jConveyorTicker.min.css'
+import Footer from '../components/Footer.js'
+import Sidemenu from '../components/Sidemenu.js'
+import Header from '../components/Header.js'
+import Loader from '../components/Loader.js'
+//if (typeof window !== "undefined") {
+  //require("jquery");
+//}
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return(
+    <>
+    
+    <Sidemenu></Sidemenu>
+    <Header></Header>
+    <Component {...pageProps} />
+    <Footer></Footer>
+    </>
+  )
 }
 
 export default MyApp
