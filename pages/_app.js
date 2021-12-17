fbb9208e4304043c2acd7672cfd86483c36a148c
artur <arturi25turi@gmail.com>
2021-12-17 11:09:31 +0100

    minor fixes

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,22 @@
-import '../styles/globals.css'
-
+import '../styles/bootstrap.min.css'
+import '../styles/animate.min.css'
+import '../styles/owl.carousel.min.css'
+import '../styles/jquery.jConveyorTicker.min.css'
+import '../styles/style.css'
+import Head from 'next/head';
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <>
+  <Head>
+   <title>Nextcoin - Bitcoin And Crypto Currency Mobile Template</title>
+    <link href="https://fonts.googleapis.com/css?family=Nunito:300,400,600,700,900" rel="stylesheet"/>
+    <link rel="icon" href="../favicon.png"/>
+    <link rel="apple-touch-icon" href="img/touch/homescreen96.png"/>
+    <link rel="apple-touch-icon" sizes="144x144" href="../touch/homescreen144.png"/>
+    <link rel="apple-touch-icon" sizes="168x168" href="../touch/homescreen168.png"/>
+    <link rel="apple-touch-icon" sizes="192x192" href="../touch/homescreen192.png"/>
+    </Head>
+<Component {...pageProps} />
+</>
 }
 
 export default MyApp
